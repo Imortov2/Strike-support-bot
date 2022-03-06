b8ce6cabbc5b000d806a948d2ab40b3788ed9730
Imortov2 <klenpuppy@gmail.com>
2022-03-06 14:05:30 -0500

    part 2

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const Discord = require ("discord.js")
 require("dotenv").config()
 
-const TOKEN = "OTQ5ODU3MDc0NjY4NTMxNzYz.YiQdVg.3vuzPze0nJUD9mej7KtsHzaPu04"
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
 client.on("ready,", () => {
-    console.log('Logged in as ${client.user.tag}')
+    console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on("messageCreate", (message) => {
     if (message.content == "salut"){
         message.reply("Salut!")
     }
+})
+
+const WelcomeChannelId = "949845477568958514"
+
+client.on("guildMemberAdd", async (member) => {
+    const Image = await generateImage(member)
+    member.guild.channels.cache.get(WelcomeChannelId).send({
+        content:`<@${member.id}> Bienvenue dans strike RP!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
